f790cdce16d6e156d3502469537282c75445e114
陈金杰 <739917425@qq.com>
2018-02-28 00:30:35 +0800

    refactor: organize the structure

--- shop_online/shopping/create_db.php
+++ shop_online/shopping/create_db.php
@@ -6,14 +6,14 @@ $link = mysqli_connect($servername, $username, $password);  // 创建连接
 if (!$link)                                                 // 检测连接
     die('Connection failed: '. mysqli_connect_error());
 echo '连接成功';
-$create='CREATE DATABASE commodity_db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci';
+$create='CREATE DATABASE shop_db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci';
 if (mysqli_query($link,$create))      //创建并检测库，设置编码格式
 	echo '创建库成功';
 else
 	echo '创建库失败'.mysqli_error($link);
-mysqli_select_db($link,'commodity_db');                            //选择库
+mysqli_select_db($link,'shop_db');                            //选择库
 //建普通用户表
-$create_usertable='CREATE TABLE user_table                               
+$create_usertable='CREATE TABLE user                               
 (
     user_id varchar(40) NOT NULL,
 	user_password varchar(40) NOT NULL,
@@ -24,7 +24,7 @@ if (mysqli_query($link,$create_usertable))
 else
 	echo '创建普通用户账号表失败'.mysqli_error($link);
 //建管理员表
-$create_managertable='CREATE TABLE manager_table                          
+$create_managertable='CREATE TABLE manager                         
 (
     manager_id varchar(40) NOT NULL,
 	manager_password varchar(40) NOT NULL,
@@ -34,17 +34,24 @@ if (mysqli_query($link,$create_managertable))
     echo '创建管理员账户表成功';
 else
 	echo '创建管理员账户表失败'.mysqli_error($link);
-//建商品信息表
-$create_commoditytable='CREATE TABLE commodity_table                          
+//建商品信息表(id,商品名，种类，描述，图片)
+$create_commoditytable='CREATE TABLE commodity                          
 (
-    commodity_id varchar(40) NOT NULL,
+    commodity_id varchar(40) NOT NULL, 
+	commodity_name vachar(20) NOT NULL, 
+	commodity_type varchar(40) NOT NULL,
 	commodity_describe varchar(120),
 	image_url varchar(120),
     PRIMARY KEY (commodity_id)	
 )';                                                         
 if (mysqli_query($link,$create_commoditytable))                              
-    echo '创建管理员账户表成功';
+    echo '创建商品表成功';
 else
-	echo '创建管理员账户表失败'.mysqli_error($link);
+	echo '创建商品表失败'.mysqli_error($link);
+//建购物车表
+$create_shopcart='CREATE TABLE shopcart
+(
+    
+)';
 mysqli_close($link);
 ?>
---
+++ shop_online/shopping/user_register.php
@@ -0,0 +1,88 @@
+<html>
+  <head>
+    <meta charset=UTF-8>
+    <title>用户注册</title>
+  </head>
+  <style type ="text/css">
+    .b_style
+	{
+		text-align:center;
+		background-color:#DFFFDF;
+		font-weight:bold;
+		font-size:15px;
+	}
+	.t_style
+	{
+		font-weight:bold;
+		font-size:35px;
+	}
+	.error
+	{
+		color:red;
+	}
+  </style>	
+  <body class="b_style">
+  <?php
+    $nameErr = $passwordErr = $a_passwordErr = "";
+	$servername = "localhost";
+    $username = "root";
+    $password = "0424";
+    $link = mysqli_connect($servername, $username, $password);  // 创建连接
+    if (!$link)                                                 // 检测连接
+        die("Connection failed: " . mysqli_connect_error());
+    mysqli_select_db($link,'shop_db');                            //选择库		
+	if ($_SERVER["REQUEST_METHOD"] == "POST")
+	{			
+			if (empty($_POST["username"]))
+				$nameErr="账号不能为空";
+            else if (!preg_match("/^[a-zA-Z0-9]+$/",$_POST["username"]))
+                $nameErr="非法账号格式";				
+            else if (empty($_POST["password"]))
+                $passwordErr="密码不能为空";
+            else if (empty($_POST["a_password"])||$_POST["password"]!=$_POST["a_password"])
+                $a_passwordErr="两次密码不一致";
+			else
+			{
+				$select="SELECT user_id FROM user WHERE user_id=".$_POST['username'];    //检测用户名是否已存在
+				$result = mysqli_query($link,$select);
+				if (mysqli_num_rows($result)>0)
+					$nameErr="用户名已存在";
+				else
+				{
+					$new_pass=md5($_POST["password"]);
+					mysqli_select_db($link,'shop_db');
+					$insert="INSERT INTO user (user_id,user_password) VALUES ('".$_POST['username']."','".$new_pass."')";
+					if (mysqli_query($link,$insert))
+					{
+						echo '注册成功';
+                    }
+                    else
+                        echo '注册失败，请重试：'.mysqli_error($link);						
+				}	
+			}	
+			    
+	}	
+  ?>
+    <h1 class="t_style"> 用户注册 </h1>  
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">  
+      用户名：<input type="text" name="username" >
+	  <span class="error">*由字母和数字组成</span>
+	  <br/>
+	  <span class="error"><?php echo $nameErr ?></span>
+      <br/>  
+      密码：<input type="password" name="password" >
+	  <span class="error">*由字母，数字和符号组成</span>
+	  <br/>
+      <span class="error"><?php echo $passwordErr ?></span>	  
+      <br/> 
+	  再次确认密码：<input type="password" name="a_password" >
+	  <span class="error">*再次输入以确认</span>
+	  <br/>
+      <span class="error"><?php echo $a_passwordErr ?></span>	  
+      <br/><br/>  
+      <input type="submit" name="submit" value="注册" >
+	  <br/><br/>
+	  <a href="login.php"><span class="b_style">返回登录界面</span></a> 
+    </form>
+  </body>	
+</html>
